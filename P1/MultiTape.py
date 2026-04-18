11848a78a9fc3d0b9661e7b0d498fcc96a106627
ValeMarcial <valentinhormiga@hotmail.com>
2026-04-18 16:54:12 -0300

    Create MultiTape

---
+++ P1/MultiTape.py
@@ -0,0 +1,104 @@
+class MultiTape:
+    def __init__(self, word, states, cant_tipes):
+        self.headers = [cant_tipes * [0]]
+        self.tapes = [cant_tipes * [[]]]
+        for i in range(len(word)):
+            self.tapes[0].append(word[i])
+        self.ends_right = [cant_tipes * [0]]
+        self.ends_right[0] = len(word)
+        self.empty_sym = "-"
+        self.states = states
+        self.actual_state = states[0]
+
+    def moveReplace(self, sym, dir, num_tipe, state):
+        """
+        This method replace symbol to actually header and change configuration
+            Args:
+                sym: Symbol to replace
+                dir: Direction to move
+                num_tipe: Number of the tape to move
+                state: State to change
+        """
+        if dir == "R":
+            if self.header_right >= 0 and self.header_left == -1:
+                if self.header_right == self.end_right:
+                    self.tape_right.append(sym)
+                    self.end_right += 1
+                else:
+                    self.tape_right[self.header_right] = sym
+                self.header_right += 1
+            elif self.header_right == -1 and self.header_left == 0:
+                self.tape_left[0] = sym
+                self.header_left = -1
+                self.header_right = 0
+            elif self.header_right == -1 and self.header_left >= 1:
+                self.tape_left[self.header_left] = sym
+                self.header_left -= 1
+        else:
+            # Move to Left
+            if self.header_right >= 1 and self.header_left == -1:
+                self.tape_right[self.header_right] = sym
+                self.header_right -= 1
+            elif self.header_right == 0 and self.header_left == -1:
+                self.tape_right[0] = sym
+                self.header_right = -1
+                self.header_left = 0
+                if self.header_left == self.end_left:
+                    self.tape_left.append(self.empty_sym)
+                    self.end_left += 1
+            elif self.header_right == -1 and self.header_left >= 0:
+                if self.header_left == self.end_left:
+                    self.tape_left.append(self.empty_sym)
+                    self.end_left += 1
+                self.tape_left[self.header_left] = sym
+                self.header_left += 1
+        self.actual_state = state
+
+    def move(self, dir, state):
+        """
+        This method move the header and change configuration
+            Args:
+                dir: Direction to move
+                state: State to change
+        """
+        if dir == "R":
+            if self.header_right >= 0 and self.header_left == -1:
+                if self.header_right == self.end_right:
+                    self.end_right += 1
+                self.header_right += 1
+            elif self.header_right == -1 and self.header_left == 0:
+                self.header_left = -1
+                self.header_right = 0
+            elif self.header_right == -1 and self.header_left >= 1:
+                self.header_left -= 1
+        else:
+            # Move to Left
+            if self.header_right >= 1 and self.header_left == -1:
+                self.header_right -= 1
+            elif self.header_right == 0 and self.header_left == -1:
+                self.header_right = -1
+                self.header_left = 0
+                if self.header_left == self.end_left:
+                    self.tape_left.append(self.empty_sym)
+                    self.end_left += 1
+            elif self.header_right == -1 and self.header_left >= 0:
+                if self.header_left == self.end_left:
+                    self.tape_left.append(self.empty_sym)
+                    self.end_left += 1
+                self.header_left += 1
+        self.actual_state = state
+
+    def getSymbol(self):
+        """
+        This method obtain symbol to actually header
+        """
+        if self.header_right == -1:
+            if self.header_left == self.end_left:
+                return self.empty_sym
+            else:
+                return self.tape_left[self.header_left]
+        else:
+            if self.header_right == self.end_right:
+                return self.empty_sym
+            else:
+                return self.tape_right[self.header_right]
